193d174cd892e145e272f8ed22dd2efe89442b6b
huangchixing2 <15007100341@163.com>
2019-03-18 22:43:53 +0800

    更多intent用法 传递结果，返回结果

--- app/src/main/java/com/example/administrator/displayintent/MainActivity.java
+++ app/src/main/java/com/example/administrator/displayintent/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.administrator.displayintent;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button bt1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +25,49 @@ public class MainActivity extends AppCompatActivity {
                 //显示intent
 //                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 //隐式intent
-                Intent intent = new Intent("com.example.administrator.displayintent.ACTION_START");
-                startActivity(intent);
+//                Intent intent = new Intent("com.example.administrator.displayintent.ACTION_START");
+                //使用intent调用系统浏览器访问指定网页
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+                //Uri.parse() 是将一个字符串解析为一个uri对象，再调用setData（）方法将uri传进去
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+                //
+                //调用系统拨号盘
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+                //向下一个activity传递数据
+                String data = "hello secondActivity";
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//                intent.putExtra("extra_data", data);
+                // startActivity(intent);
+                //返回数据给上一个活动
+                startActivityForResult(intent, 1);
+
 
             }
         });
     }
+
+    /**
+     *
+     *
+     * @param requestCode 启动活动时传入的请求码
+     * @param resultCode 返回数据时传入的处理结果
+     * @param data 携带者返回数据的intent
+     */
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnData = data.getStringExtra("data_return");
+                    Log.d("FirstAcitivity", returnData);
+                }
+                break;
+            default:
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/administrator/displayintent/SecondActivity.java
+++ app/src/main/java/com/example/administrator/displayintent/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.example.administrator.displayintent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+//        //取出第一个activity传递过来的数据
+//        Intent intent = getIntent();
+//        String data = intent.getStringExtra("extra_data");
+//        Log.d("SecondActivity",data);
+//        Button button2 = (Button) findViewById(R.id.button2);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.putExtra("data_return", "helloFirstActivity");
+//                setResult(RESULT_OK, intent);//专门用于向上一个活动返回数据，第一个参数用于向上一个活动返回处理结果，第二个参数把带有数据的intent传递回去
+//                finish();//销毁当前活动
+//            }
+//        });
+
+    }
+
+    //back键返回数据
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "helloFirstActivity");
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
