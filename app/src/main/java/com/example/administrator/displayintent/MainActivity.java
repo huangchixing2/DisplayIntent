653b7323a5dccfd541d4f94ade4e88efd938eee2
huangchixing2 <15007100341@163.com>
2019-03-18 08:18:37 +0800

    显示intent用法

---
+++ app/src/main/java/com/example/administrator/displayintent/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.administrator.displayintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt1 = (Button) findViewById(R.id.button1);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
